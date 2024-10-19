609bde77ac70855bee1508291da87c815aea92bd
BS <kwanghochoi@hotmail.com>
2024-10-19 22:59:39 +0900

    fix cors bug

--- internal/server/cors.go
+++ internal/server/cors.go
@@ -6,14 +6,15 @@ import (
 	"strings"
 )
 
+const allowedMethods = "GET, POST, PUT, DELETE, OPTIONS"
+
 func handleCORS(cfg *config.CORSConfig, w http.ResponseWriter, r *http.Request) bool {
 	w.Header().Set("Access-Control-Allow-Origin", cfg.AllowOrigin)
+	w.Header().Set("Access-Control-Allow-Methods", allowedMethods)
+	w.Header().Set("Access-Control-Allow-Headers", "*")
 
 	if r.Method == http.MethodOptions {
-		w.Header().Set("Access-Control-Allow-Methods", "GET, PUT, DELETE, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "*")
-
-		if !strings.Contains("GET, PUT, DELETE, OPTIONS", r.Header.Get("Access-Control-Request-Method")) {
+		if !strings.Contains(allowedMethods, r.Header.Get("Access-Control-Request-Method")) {
 			HttpErr(w, http.StatusMethodNotAllowed)
 			return true
 		}
